180e04b160ecc926a01004edd22f334de2d3830a
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2021-04-28 15:15:58 -0400

    database accomplish

--- database/database.sql
+++ database/database.sql
@@ -11,39 +11,39 @@ CREATE TABLE account (
      lastName varchar(30) NOT NULL,
      email varchar(100) NOT NULL,
      passphrase varchar(255) NOT NULL,
-     state_id int,
-     permission_id int,
-     school varchar(100),
-     birthday DATE NOT NULL,
-     gender_id int NOT NULL,
-     location_id int NOT NULL,
-     team_id int NOT NULL
+     state_id int NOT NULL,
+     permission_id int NOT NULL,
+     school varchar(100) NOT NULL,
+     birthday DATE,
+     gender_id int,
+     team_id int
 );
 
 CREATE TABLE state (
      id int PRIMARY KEY  AUTO_INCREMENT,
-     title varchar(100)
+     title varchar(100) NOT NULL
 );
 
 CREATE TABLE permission (
      id INT PRIMARY KEY  AUTO_INCREMENT,
-     title varchar(100)
+     title varchar(100) NOT NULL
 );
 
 CREATE TABLE gender (
      id INT PRIMARY KEY  AUTO_INCREMENT,
-     genderType varchar(100)
+     genderType varchar(100) NOT NULL
 );
 
 CREATE TABLE location (
      id INT PRIMARY KEY  AUTO_INCREMENT,
      country varchar(100) NOT NULL,
      state varchar(100) NOT NULL,
-     city varchar(100)NOT NULL,
-     address varchar(100)NOT NULL,
+     city varchar(100) NOT NULL,
+     address varchar(100) NOT NULL,
      civicNumber int NOT NULL,
      postalCode int NOT NULL,
-     appartNumber int
+     appartNumber int,
+     account_id int NOT NULL
 );
 
 CREATE TABLE team (
@@ -68,9 +68,12 @@ ALTER TABLE account
     ADD FOREIGN KEY (state_id) REFERENCES state (id),
     ADD FOREIGN KEY (permission_id) REFERENCES permission (id),
     ADD FOREIGN KEY (gender_id) REFERENCES gender (id),
-    ADD FOREIGN KEY (location_id) REFERENCES location (id),
     ADD FOREIGN KEY (team_id) REFERENCES team (id);
 
 ALTER TABLE team
-  ADD FOREIGN KEY (event_id) REFERENCES codingEvent (id);
+  ADD FOREIGN KEY (event_id) REFERENCES codingEvent (id) ON DELETE CASCADE;
+
+ALTER TABLE location
+  ADD FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE;
+  
 -- -----------------------------------------------------------------------------------------------
--- database/test.sql
+++ database/test.sql
@@ -1,3 +1,12 @@
 /*
 SQL test for uqode
 */
+INSERT INTO codingEvent (name,startDate,endDate,description) VALUES ("Evenement de test",'2021-02-23 02:40:31', '2021-02-23 02:40:32',"je suis une description");
+INSERT INTO team (name,numberParticipants,event_id) VALUES ("Les bananes", 4,1);
+
+DELETE FROM codingEvent WHERE id = 1;
+
+INSERT INTO location (country, account_id) VALUES ("ta mere",1);
+INSERT INTO account (firstname,lastname, email, passphrase) VALUES ("Jean", "JJ","kfwrf@gmai.com", "ejfwn");
+
+DELETE FROM account WHERE id=1;
