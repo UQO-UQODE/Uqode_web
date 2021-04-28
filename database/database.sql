52c51400e44e176144ded72c9bfe66e5fc8d5327
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2021-04-28 13:47:39 -0400

    finish sql script

--- database/database.sql
+++ database/database.sql
@@ -1,67 +1,76 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 27, 2021 at 09:37 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+/*
+UQODE Database
+RDBMS: mySQL
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE Database IF NOT EXISTS uqode_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+CREATE TABLE account (
+     id INT PRIMARY KEY  AUTO_INCREMENT,
+     firstName varchar(30) NOT NULL,
+     lastName varchar(30) NOT NULL,
+     email varchar(100) NOT NULL,
+     passphrase varchar(255) NOT NULL,
+     state_id int,
+     permission_id int,
+     school varchar(100),
+     birthday DATE NOT NULL,
+     gender_id int NOT NULL,
+     location_id int NOT NULL,
+     team_id int NOT NULL
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE state (
+     id int PRIMARY KEY  AUTO_INCREMENT,
+     title varchar(100)
+);
 
---
--- Database: `nodejs-login`
---
+CREATE TABLE permission (
+     id INT PRIMARY KEY  AUTO_INCREMENT,
+     title varchar(100)
+);
 
--- --------------------------------------------------------
+CREATE TABLE gender (
+     id INT PRIMARY KEY  AUTO_INCREMENT,
+     genderType varchar(100)
+);
 
---
--- Table structure for table `users`
---
+CREATE TABLE location (
+     id INT PRIMARY KEY  AUTO_INCREMENT,
+     country varchar(100) NOT NULL,
+     state varchar(100) NOT NULL,
+     city varchar(100)NOT NULL,
+     address varchar(100)NOT NULL,
+     civicNumber int NOT NULL,
+     postalCode int NOT NULL,
+     appartNumber int
+);
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE team (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    name varchar(30) NOT NULL,
+    numberParticipants int NOT NULL,
+    event_id int NOT NULL
+);
 
---
--- Dumping data for table `users`
---
+CREATE TABLE codingEvent (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    startDate datetime NOT NULL,
+    endDate datetime NOT NULL,
+    description text NOT NULL,
+    location_id int NOT NULL
+);
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'asd', 'daninajjar@yahoo.com', '$2a$08$KWreabpOhNRcjU4LYqK1kOknsMqbz9/68e.vVvv/3E3');
+-- ------ADDING THE FOREIGN KEY--------
 
---
--- Indexes for dumped tables
---
+ALTER TABLE account
+    ADD FOREIGN KEY (state_id) REFERENCES state (id),
+    ADD FOREIGN KEY (permission_id) REFERENCES permission (id),
+    ADD FOREIGN KEY (gender_id) REFERENCES gender (id),
+    ADD FOREIGN KEY (location_id) REFERENCES location (id),
+    ADD FOREIGN KEY (team_id) REFERENCES team (id);
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE team
+  ADD FOREIGN KEY (event_id) REFERENCES codingEvent (id);
+-- -----------------------------------------------------------------------------------------------
