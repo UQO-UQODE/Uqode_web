356d92e2c7e774ad5b00221a285d151436885b7b
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2021-05-15 20:53:06 -0400

    organize router.js

--- routes/router.js
+++ routes/router.js
@@ -6,26 +6,12 @@ const val = require('../validation/validate').validate;
 const controlGuess = require('../controllers/guess');
 const controlAdmin = require('../controllers/admin');
 const {check, validationResult} = require('express-validator');
-/*
-function test(){
-  console.log('the test');
-  return [
-    check('email').isEmail(),
-    check('email').exists()
-  ]
 
-}*/
+
 /*Guess routes*/
 router.get(['/','/home'], function (req, res) {
   res.render('HomePage');
 });
-
-router.get('/login', function (req, res) {
-  res.render('connexion');
-});
-
-
-
 
 router.get('/faq', function (req, res) {
   res.render('faq');
@@ -39,26 +25,21 @@ router.get('/contact-us', function (req, res) {
   res.render('contact-us');
 });
 
+router.post('/createUser',val('user'),controlGuess.createUser);
+
+/*users routes */
+
+router.get('/login', function (req, res) {
+  res.render('connexion');
+});
+
 router.get('/projects', function (req, res) {
   res.render('projects');
 });
 
-router.post('/createUser',val('user'),controlGuess.createUser);
-
-
+/*admin routes */
 
 router.post('/event_create', controlAdmin.createEvent);
 
-  
-
-
-
-
-/*users routes */
-
-/*admin routes */
-
 
 module.exports = router;
-
-
