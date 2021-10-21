a4608d572de7c4d429ef4803ea0cb85d259cfb93
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2021-10-21 16:34:26 -0400

    finally use .env file in the db connection

--- database/db_connect_V3.js
+++ database/db_connect_V3.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
 const connect = ()=>{
-  return new Sequelize('uqode_db', 'root', '', {
-    host: 'localhost',
-    dialect:  'mysql',
+  return new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect:  process.env.DB_DBMS,
   });
   
 }
